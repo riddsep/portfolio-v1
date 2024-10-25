1da89068c0ce8b8e2882fa1bfcd121e7a73d8455
riddsep <ridoseptiawan00@gmail.com>
2024-10-26 00:44:17 +0700

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Moon, Menu, ArrowUpRight, Github, Paperclip } from "lucide-react";
 import { useState } from "react";
-import "./scss/style.scss";
+import "../scss/style.scss";
 function App() {
   const [toggle, setToggle] = useState(false);
   return (
